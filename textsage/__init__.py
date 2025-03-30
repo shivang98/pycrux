c9961df34c37fba2fcbf1ddcf5d02816c645fffd
Shivang Agarwal <shivangagarwal98@gmail.com>
2025-03-30 22:28:00 +0530

    minor fix

--- textsage/__init__.py
+++ textsage/__init__.py
@@ -1,6 +1,6 @@
 """
 TextSage is a Python library that sets up Ollama and local LLMs for users and 
-provides a simple function to summarize text.
+provides a simple function to analyse text.
 """
 
 from .llm_utils import summarize_text, analyze_sentiment, extract_key_phrases
